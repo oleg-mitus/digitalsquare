e729dc0993dece9e8fe32c62d5ce643919d60294
Oleg Mitus <mitusos@melonfashion.com>
2026-04-22 18:23:15 +0300

    add mixins to additional data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,10 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
+          additionalData: `
+            @use "~/assets/scss/_variables.scss" as *;
+            @use "~/assets/scss/_mixins.scss" as *;
+          `,
         },
       },
     },
